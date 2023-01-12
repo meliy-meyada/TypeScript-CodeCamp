d721b383fbf4e2d0429fe25da8570751bd5831d2
Meliy <79361511+meliy-meyada@users.noreply.github.com>
2023-01-12 14:44:44 +0700

    Another Way

--- ts-camp/app.js
+++ ts-camp/app.js
@@ -1,17 +1,17 @@
 function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
-    if (resultConversion === 'as-number') {
-        return +result;
-    }
-    else {
-        return result.toString();
-    }
+    return result;
+    // if(resultConversion === 'as-number'){
+    //   return +result;
+    // } else {
+    //   return result.toString();
+    // }
 }
 var combineAges = combine(26, 40, 'as-number');
 console.log(combineAges);
--- ts-camp/app.ts
+++ ts-camp/app.ts
@@ -1,19 +1,20 @@
 function combine(
   input1: number | string, 
   input2: number | string, 
-  resultConversion: string
+  resultConversion: 'as-number' | 'as-text'
   ){
   let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number'){
-    result = input1 + input2;
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number' ){
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-  if(resultConversion === 'as-number'){
-    return +result;
-  } else {
-    return result.toString();
-  }
+  return result;
+  // if(resultConversion === 'as-number'){
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
 }
 
 const combineAges = combine(26, 40, 'as-number');
