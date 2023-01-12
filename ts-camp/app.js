b740cd0e352432c697dd5a8e3f39ff48576f4e37
Meliy <79361511+meliy-meyada@users.noreply.github.com>
2023-01-12 14:36:22 +0700

    literal types

--- ts-camp/app.js
+++ ts-camp/app.js
@@ -1,4 +1,4 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
     if (typeof input1 === 'number' && typeof input2 === 'number') {
         result = input1 + input2;
@@ -6,9 +6,16 @@ function combine(input1, input2) {
     else {
         result = input1.toString() + input2.toString();
     }
-    return result;
+    if (resultConversion === 'as-number') {
+        return +result;
+    }
+    else {
+        return result.toString();
+    }
 }
-var combineAges = combine(26, 40);
+var combineAges = combine(26, 40, 'as-number');
 console.log(combineAges);
-var combineName = combine('Meya', 'Anne');
+var combineStringAges = combine('26', '40', 'as-number');
+console.log(combineStringAges);
+var combineName = combine('Meya', 'Anne', 'as-text');
 console.log(combineName);
--- ts-camp/app.ts
+++ ts-camp/app.ts
@@ -1,15 +1,26 @@
-function combine(input1: number | string, input2: number | string){
+function combine(
+  input1: number | string, 
+  input2: number | string, 
+  resultConversion: string
+  ){
   let result;
   if (typeof input1 === 'number' && typeof input2 === 'number'){
     result = input1 + input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-    return result;
+  if(resultConversion === 'as-number'){
+    return +result;
+  } else {
+    return result.toString();
+  }
 }
 
-const combineAges = combine(26, 40);
+const combineAges = combine(26, 40, 'as-number');
 console.log(combineAges);
 
-const combineName = combine('Meya', 'Anne')
+const combineStringAges = combine('26', '40', 'as-number');
+console.log(combineStringAges);
+
+const combineName = combine('Meya', 'Anne', 'as-text')
 console.log(combineName);
