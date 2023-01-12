ebbf7eaa4a3f3cf75c73adb29cdf2ea69b6697f8
Meliy <79361511+meliy-meyada@users.noreply.github.com>
2023-01-12 16:37:58 +0700

    Function as Types

--- ts-camp/app.js
+++ ts-camp/app.js
@@ -3,6 +3,10 @@ function add(n1, n2) {
 }
 function printResult(num) {
     console.log('Result: ' + num);
-    return;
 }
 printResult(add(5, 23));
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+console.log(combineValues(8, 8));
--- ts-camp/app.ts
+++ ts-camp/app.ts
@@ -2,10 +2,17 @@ function add(n1: number, n2: number){
   return n1 + n2;
 }
 
-function printResult(num: number): undefined {
+function printResult(num: number): void {
   console.log('Result: ' + num);
-  return;
+  
 }
-
 printResult(add(5, 23));
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+
+console.log(combineValues(8, 8));
 
