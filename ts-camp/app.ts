9317de682b3f432d6da7ba14364689cc097119d1
Meliy <79361511+meliy-meyada@users.noreply.github.com>
2023-01-12 13:19:37 +0700

    working with enum

--- ts-camp/app.ts
+++ ts-camp/app.ts
@@ -2,18 +2,34 @@
 //   name: string;
 //   age: number;
 // } = {
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Meyada',
+//   age: 26,
+//   hobbies: ['Sports', 'Coding'],
+//   role: [2, 'author']
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1; 
+// const AUTHOR = 2;
+
+enum Role {ADMIN, READ_ONLY, AUTHOR };
+
+const person = {
   name: 'Meyada',
   age: 26,
   hobbies: ['Sports', 'Coding'],
-  role: [2, 'author']
+  role: Role.ADMIN
 };
-person.role.push('admin');
+// person.role.push('admin');
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user']; 
 
 let favoriteAc: string[];
 favoriteAc = ['Coding'];
@@ -23,4 +39,8 @@ console.log(person.name);
 for(const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
   // console.log(hobby.map()); !!ERROR!!
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is author');
 }
