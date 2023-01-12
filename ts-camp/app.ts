58ca892b80c900ee473e6b441f4140c5295e5233
Meliy <79361511+meliy-meyada@users.noreply.github.com>
2023-01-12 15:08:08 +0700

    Type Aliases and Custom Type

--- ts-camp/app.ts
+++ ts-camp/app.ts
@@ -1,7 +1,10 @@
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
 function combine(
-  input1: number | string, 
-  input2: number | string, 
-  resultConversion: 'as-number' | 'as-text'
+  input1: Combinable, 
+  input2: Combinable, 
+  resultConversion: ConversionDescriptor
   ){
   let result;
   if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number' ){
