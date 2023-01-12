c47ea6731dec5a1b8eb6570f7101d1074830c680
Meliy <79361511+meliy-meyada@users.noreply.github.com>
2023-01-12 16:14:29 +0700

    Function Return Types & void

--- ts-camp/app.js
+++ ts-camp/app.js
@@ -1,21 +1,8 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if(resultConversion === 'as-number'){
-    //   return +result;
-    // } else {
-    //   return result.toString();
-    // }
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combineAges = combine(26, 40, 'as-number');
-console.log(combineAges);
-var combineStringAges = combine('26', '40', 'as-number');
-console.log(combineStringAges);
-var combineName = combine('Meya', 'Anne', 'as-text');
-console.log(combineName);
+function printResult(num) {
+    console.log('Result: ' + num);
+    return;
+}
+printResult(add(5, 23));
--- ts-camp/app.ts
+++ ts-camp/app.ts
@@ -1,30 +1,11 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
-
-function combine(
-  input1: Combinable, 
-  input2: Combinable, 
-  resultConversion: ConversionDescriptor
-  ){
-  let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number' ){
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  // if(resultConversion === 'as-number'){
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
+function add(n1: number, n2: number){
+  return n1 + n2;
 }
 
-const combineAges = combine(26, 40, 'as-number');
-console.log(combineAges);
+function printResult(num: number): undefined {
+  console.log('Result: ' + num);
+  return;
+}
 
-const combineStringAges = combine('26', '40', 'as-number');
-console.log(combineStringAges);
+printResult(add(5, 23));
 
-const combineName = combine('Meya', 'Anne', 'as-text')
-console.log(combineName);
