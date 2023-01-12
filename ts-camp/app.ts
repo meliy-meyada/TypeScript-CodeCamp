c96fc7f69c5fbf4e31008711b7e9ebfea63b7a5a
Meliy <79361511+meliy-meyada@users.noreply.github.com>
2023-01-12 14:17:28 +0700

    Union Types

--- ts-camp/app.ts
+++ ts-camp/app.ts
@@ -1,46 +1,15 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string];
-// } = {
-//   name: 'Meyada',
-//   age: 26,
-//   hobbies: ['Sports', 'Coding'],
-//   role: [2, 'author']
-// };
-
-// const ADMIN = 0;
-// const READ_ONLY = 1; 
-// const AUTHOR = 2;
-
-enum Role {ADMIN, READ_ONLY, AUTHOR };
-
-const person = {
-  name: 'Meyada',
-  age: 26,
-  hobbies: ['Sports', 'Coding'],
-  role: Role.ADMIN
-};
-// person.role.push('admin');
-// person.role[1] = 10;
-
-// person.role = [0, 'admin', 'user']; 
-
-let favoriteAc: string[];
-favoriteAc = ['Coding'];
-
-console.log(person.name);
-
-for(const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-  // console.log(hobby.map()); !!ERROR!!
+function combine(input1: number | string, input2: number | string){
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number'){
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+    return result;
 }
 
-if (person.role === Role.AUTHOR) {
-  console.log('is author');
-}
+const combineAges = combine(26, 40);
+console.log(combineAges);
+
+const combineName = combine('Meya', 'Anne')
+console.log(combineName);
