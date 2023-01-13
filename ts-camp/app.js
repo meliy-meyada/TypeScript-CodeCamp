2a2da450e947869379f17efed5b1d7c1c90edeaa
Meliy <79361511+meliy-meyada@users.noreply.github.com>
2023-01-13 15:18:27 +0700

    The "never" Type

--- ts-camp/app.js
+++ ts-camp/app.js
@@ -5,3 +5,8 @@ userName = 'Meya';
 if (typeof userInput === 'string') {
     userName = userInput;
 }
+function generateErr(message, code) {
+    throw { message: message, errorCode: code };
+    // while (true) {}
+}
+generateErr('An error occurred', 500);
--- ts-camp/app.ts
+++ ts-camp/app.ts
@@ -7,3 +7,10 @@ userName = 'Meya';
 if (typeof userInput === 'string') {
   userName = userInput;
 }
+
+function generateErr(message: string, code: number): never {
+  throw {message: message, errorCode: code};
+  // while (true) {}
+}
+
+generateErr('An error occurred', 500);
