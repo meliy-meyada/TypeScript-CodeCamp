421f2fca52e3ef7bc797031d9dbd4996f07a8b74
Meliy <79361511+meliy-meyada@users.noreply.github.com>
2023-01-13 15:06:08 +0700

    The "unknown" Type

--- ts-camp/app.js
+++ ts-camp/app.js
@@ -1,20 +1,7 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userName = 'Meya';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log('Result: ' + num);
-}
-function addHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 23));
-var combineValues;
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-console.log(combineValues(8, 8));
-addHandle(10, 30, function (result) {
-    console.log(result);
-    return result;
-});
--- ts-camp/app.ts
+++ ts-camp/app.ts
@@ -1,28 +1,9 @@
-function add(n1: number, n2: number){
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+
+userInput = 5;
+userName = 'Meya';
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
-
-function printResult(num: number): void {
-  console.log('Result: ' + num);
-  
-}
-function addHandle(n1: number, n2: number, cb: (num: number) => void){
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResult(add(5, 23));
-
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-
-console.log(combineValues(8, 8));
-
-addHandle(10, 30, (result) => {
-  console.log(result);
-  return result;
-})
-
