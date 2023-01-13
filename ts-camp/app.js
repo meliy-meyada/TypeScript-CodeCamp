f018d8decb061e3d7ad2ac02140d82fe996d46c7
Meliy <79361511+meliy-meyada@users.noreply.github.com>
2023-01-13 14:42:15 +0700

    Function Types & Callbacks

--- ts-camp/app.js
+++ ts-camp/app.js
@@ -4,9 +4,17 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result: ' + num);
 }
+function addHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printResult(add(5, 23));
 var combineValues;
 combineValues = add;
 // combineValues = printResult;
 // combineValues = 5;
 console.log(combineValues(8, 8));
+addHandle(10, 30, function (result) {
+    console.log(result);
+    return result;
+});
--- ts-camp/app.ts
+++ ts-camp/app.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
   console.log('Result: ' + num);
   
 }
+function addHandle(n1: number, n2: number, cb: (num: number) => void){
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 23));
 
 let combineValues: (a: number, b: number) => number;
@@ -15,4 +20,9 @@ combineValues = add;
 // combineValues = 5;
 
 console.log(combineValues(8, 8));
+
+addHandle(10, 30, (result) => {
+  console.log(result);
+  return result;
+})
 
