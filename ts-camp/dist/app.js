dc0f248e1e89bc70cfd51bab05b0d5ed0b17d10d
Meliy <79361511+meliy-meyada@users.noreply.github.com>
2023-01-15 14:51:46 +0700

    Code quality options

--- ts-camp/dist/app.js
+++ ts-camp/dist/app.js
@@ -1,7 +1,12 @@
 "use strict";
 const button = document.querySelector('button');
+function add(n1, n2) {
+    if (n1 + n2 > 0) {
+        return n1 + n2;
+    }
+    return;
+}
 function clickHandler(msg) {
-    let userName = 'Meyade';
     console.log('Clicked!' + msg);
 }
 if (button) {
--- ts-camp/src/app.ts
+++ ts-camp/src/app.ts
@@ -1,7 +1,14 @@
 const button = document.querySelector('button')!;
 
+function add(n1: number, n2: number){
+  if(n1 + n2 > 0){
+    return n1 + n2;
+  }
+  return;
+}
+
 function clickHandler(msg: string){
-  let userName = 'Meyade';
+  // let userName = 'Meyade';
   console.log('Clicked!'+ msg);
 }
 // test comment
