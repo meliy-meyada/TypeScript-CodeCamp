16cf580d894c3aa9ce051dda09664457ffa31913
Meliy <79361511+meliy-meyada@users.noreply.github.com>
2023-01-16 13:37:20 +0700

    Arrow Functions

--- next-gen-01/src/app.ts
+++ next-gen-01/src/app.ts
@@ -4,12 +4,22 @@ let age = 26
 
 age = 25
 
-function add (a: number, b: number){
-  let result;
-  result = a + b;
-  return result;
-}
+// function add (a: number, b: number){
+//   let result;
+//   result = a + b;
+//   return result;
+// }
 // if (age > 20){
 //   let isOld = true;
 // }
 // console.log(isOld);
+const add = (a: number, b: number) =>  a + b;
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+
+if (button) {
+  button.addEventListener('click', event => console.log(event));
+}
+printOutput(add(5, 2)); 
