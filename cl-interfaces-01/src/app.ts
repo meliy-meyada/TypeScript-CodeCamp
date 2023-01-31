9c09468e7d7618f877edd3dc38a993725afac69a
Meliy <79361511+meliy-meyada@users.noreply.github.com>
2023-01-31 14:16:37 +0700

    Extending Interfaces

--- cl-interfaces-01/src/app.ts
+++ cl-interfaces-01/src/app.ts
@@ -1,10 +1,12 @@
-interface Greetable {
-  name: string;
+interface Named {
+  readonly name: string;
 
+}
+interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
-class Person implements Greetable {
+class Person implements Greetable, Named {
   name: string;
   age = 26;
 
