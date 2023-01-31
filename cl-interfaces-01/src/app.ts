a855b3e2de6b72836576d956efe54257a12649c8
Meliy <79361511+meliy-meyada@users.noreply.github.com>
2023-01-31 14:26:58 +0700

    Interfaces as Function Types

--- cl-interfaces-01/src/app.ts
+++ cl-interfaces-01/src/app.ts
@@ -1,3 +1,13 @@
+// type AddFn = ( a: number,  b: number ) => number;
+interface AddFn {
+  ( a: number, b: number ): number;
+}
+let add: AddFn;
+
+add = ( n1: number, n2: number ) => {
+  return n1 + n2;
+}
+
 interface Named {
   readonly name: string;
 
