94cc2c00e4f1af190b3d75abc68969baf2edada0
Meliy <79361511+meliy-meyada@users.noreply.github.com>
2023-02-03 12:27:35 +0700

    Index Properties

--- adv-types-01/src/app.ts
+++ adv-types-01/src/app.ts
@@ -102,7 +102,17 @@ moveAnimal({type: 'bird', flyingSpeed: 10});
 
 // const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
 const userInputElement = document.getElementById('user-input');
+
 if ( userInputElement ){
   (userInputElement as HTMLInputElement).value = 'Hi, There!';
   
 }
+
+interface ErrorContainer { // {email: 'Not a valid email', username: 'Must start with character!'}
+  [prop: string]: string;
+}
+const errorBag: ErrorContainer = {
+  email: 'Not a valid emali!',
+  username: 'Must start with a capital character!'
+};
+
