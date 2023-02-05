7d02bb3b304671d8f46dbe83ef07ecb99b14435e
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2023-02-05 18:40:46 +0700

    "Autobind" Decorator

--- decorators-01/src/app.ts
+++ decorators-01/src/app.ts
@@ -89,3 +89,34 @@ class Product {
 }
 const p1 = new Product('Book', 190);
 const p2 = new Product('Book 2', 290);
+
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor){
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+
+    get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+    }
+  };
+  return adjDescriptor;
+}
+
+class Printer {
+  message = "This works!";
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+
+  }
+}
+
+const p = new Printer();
+p.showMessage();
+
+const button  = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage);
