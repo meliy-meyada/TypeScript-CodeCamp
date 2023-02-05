9ea4cc39ebe3cfdd0c7e1a1fb9a22e254baa8719
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2023-02-05 17:34:05 +0700

    Accessor & Parameter Decorators

--- decorators-01/src/app.ts
+++ decorators-01/src/app.ts
@@ -41,12 +41,34 @@ function Log(target: any, propertyName: string | Symbol ) {
   console.log(target, propertyName);
 }
 
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Accessor Decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3 (target: any, name: string | Symbol, descriptor: PropertyDescriptor){
+  console.log('Method decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+console.log('Parameter  decorator!');
+console.log(target);
+console.log(name);
+console.log(position);
+}
+
 
 class Product {
   @Log
   title: string;
   private _price: number;
-
+  
+  @Log2
   set price(val: number){
     if(val > 0){
       this._price = val;
@@ -58,8 +80,8 @@ class Product {
     this.title = t;
     this._price = p;
   }
-
-  getPriceWithTax(tax: number){
+  @Log3
+  getPriceWithTax(@Log4 tax: number){
     return this._price * (1 + tax);
   }
 }
