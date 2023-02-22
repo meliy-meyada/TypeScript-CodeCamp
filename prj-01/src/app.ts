3c368cf806e0bf1dacb69bcdd9fd60b578367bdd
Meliy <meyada.saisan@gmail.com>
2023-02-22 16:56:00 +0700

    Finishing Drag & Drop

--- prj-01/src/app.ts
+++ prj-01/src/app.ts
@@ -65,6 +65,18 @@ class ProjectState extends State<Project> {
       ProjectStatus.Active
     );
     this.projects.push(newProject);
+    this.updateListeners();
+  }
+  
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.projects.find(prj => prj.id === projectId);
+    if(project && project.status !== newStatus) {
+      project.status = newStatus;
+      this.updateListeners();
+    }
+  }
+
+  private updateListeners() {
     for(const listenerFn of this.listeners) {
       listenerFn(this.projects.slice());
     }
@@ -180,7 +192,8 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements 
   }
   @autobind
   dragStartHandler(event: DragEvent) {
-    console.log(event);
+    event.dataTransfer!.setData('text/plain', this.project.id);
+    event.dataTransfer!.effectAllowed = 'move';
   }
   dragEndHandler(_: DragEvent) {
     console.log('DragEnd')
@@ -213,16 +226,25 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
   }
   
   @autobind
-  dragOverHandler(_: DragEvent) {
-    const listEl = this.element.querySelector('ul')!;
-    listEl.classList.add('droppable');
+  dragOverHandler(event: DragEvent) {
+    if(event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+      event.preventDefault();
+      const listEl = this.element.querySelector('ul')!;
+      listEl.classList.add('droppable');
+      
+    }
   }
-  dropHandler(_: DragEvent) {
-
+  dropHandler(event: DragEvent) {
+    const prjId = event.dataTransfer!.getData('text/plain');
+    projectState.moveProject(
+      prjId,
+      this.type === 'active' ?  ProjectStatus.Active : ProjectStatus.Finished 
+      );
   }
 
   @autobind
   dragLeaveHandler(_: DragEvent) {
+
     const listEl = this.element.querySelector('ul')!;
     listEl.classList.remove('droppable');
     
