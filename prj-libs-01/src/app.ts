07ef206ae10ba45d83961a52c458749e992070ba
Meliy <meyada.saisan@gmail.com>
2023-03-08 15:50:38 +0700

    Rendering a Map with Google Maps

--- prj-libs-01/src/app.ts
+++ prj-libs-01/src/app.ts
@@ -1,11 +1,48 @@
+import axios from 'axios';
+import { GOOGLE_API_KEY } from './config';
+
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+
+declare var google: any;
+type GoogleGeoCodingResponse = {
+  results: {geometry: {location: {lat: number, lng: number}}}[];
+  status: 'OK'| 'ZERO_RESULTS';
+}
+
+
+
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
   // send this to Google's APIs
+  axios
+  .get<GoogleGeoCodingResponse>(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+    enteredAddress
+    )}&key=${GOOGLE_API_KEY}`
+  )
+  .then(res => {
+    if( res.data.status !== "OK" ){
+      throw new Error('Could not fetch location!')
+    }
+    const coordinates = res.data.results[0].geometry.location;
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: coordinates,
+      zoom: 16
+    });
+    new google.maps.Marker({position: coordinates, map: map});
+  })
+  .catch(err => {
+    alert(err.message);
+    console.log(err)
+  });
+  
+
 
 }
 
