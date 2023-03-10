96a249a47e8c07af199ad111e26479cb837ac3d3
Meliy <meyada.saisan@gmail.com>
2023-03-10 14:08:10 +0700

    Getting User Input with refs

--- retypescript/src/App.tsx
+++ retypescript/src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
 
 const App: React.FC = () => {
   const todos = [{ id: 't1', text: 'Finish the course' }];
   return (
     <div className="App">
-      {/* A component that adds todos */}
-
+      <NewTodo/>
       <TodoList items={todos} />
 
     </div>
