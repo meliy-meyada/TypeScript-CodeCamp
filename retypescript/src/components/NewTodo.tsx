837bbe2c1d57d75562a9d5e196ea535dcab29bfd
Meliy <meyada.saisan@gmail.com>
2023-03-11 12:37:12 +0700

    Adding Styling

--- retypescript/src/components/NewTodo.tsx
+++ retypescript/src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import './NewTodo.css'
 
 
 type NewTodoProps = {
@@ -17,7 +18,7 @@ const NewTodo: React.FC<NewTodoProps> = props => {
 
   return (
     <form onSubmit={todoSubmitHandler}>
-      <div>
+      <div className="form-control">
           <label htmlFor="todo-text"> Todo Text </label>
           <input type="text" id="todo-text" ref={textInputRef}/>
       </div>
