82326247563240290ea8bcb043883d852e0a05af
Meliy <meyada.saisan@gmail.com>
2023-03-11 16:09:13 +0700

    Node Express Typescript

--- node-withtypescript/src/controllers/todos.ts
+++ node-withtypescript/src/controllers/todos.ts
@@ -16,3 +16,31 @@ export const createTodo: RequestHandler = (req, res, next) => {
 export const getTodos: RequestHandler = ( req, res, next ) => {
   res.json({todos: TODOS});
 };
+
+export const updateTodo: RequestHandler<{id: string}> = ( req, res, next ) => {
+  const todoId = req.params.id;
+
+  const updateText = ( req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex(todo => todo.id === todoId)
+;
+
+if(todoIndex < 0) {
+  throw new Error('Could not fins todo!'); 
+  }
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updateText);
+
+  res.json({ message: 'Updated!', updateTodo: TODOS[todoIndex]});
+
+};
+export const deleteTodo: RequestHandler = (req, res, next) => {
+  const todoId = req.params.id;
+  const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+  if(todoIndex < 0) {
+  throw new Error('Could not fins todo!'); 
+  }
+  TODOS.slice(todoIndex, 1);
+  res.json({ message: 'Todo deleted!' });
+};
+
